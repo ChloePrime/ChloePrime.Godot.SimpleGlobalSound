0296395bc4ffc3f86e5005292abcdf8a16b08da3
Chloe Prime <chloe_von_einzbern@qq.com>
2026-01-31 20:01:42 +0800

    Add README

---
+++ README.md
@@ -0,0 +1,4 @@
+Simple global sound player for Godot,<br>
+that lets you play sound anywhere without AudioStreamPlayer(2D/3D) nodes.<br>
+<br>
+See [example.tscn](example.tscn) and [example.gd](example.gd) for usage example.
--- example.gd
+++ example.gd
@@ -7,6 +7,6 @@ signal space_key_pressed
 func _input(event: InputEvent) -> void:
 	if (event.is_pressed() && event is InputEventKey):
 		if (event.keycode == KEY_SPACE):
-			space_key_pressed.emit()
-		elif (event.keycode == KEY_ENTER):
 			GlobalSoundManager.Play(audio_stream)
+		elif (event.keycode == KEY_ENTER):
+			space_key_pressed.emit()
